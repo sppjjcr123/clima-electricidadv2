69abd8f09736bb88d016a70049a794c06ed4cf61
sppjjcr123 <sppjjcr@gmail.com>
2026-08-30 20:19:31 -0400

    Preparar integración Firebase para galería
    - Agregado firebase-loader.js
    - Agregado gallery-firebase.js
    - Agregado firebase-config.js.example
    - Configurado .gitignore
    - Creado script.backup.js
    - Conectado gallery-firebase.js a index.html
    - Validado funcionamiento con Live Server

---
+++ js/gallery-firebase.js
@@ -0,0 +1,14 @@
+import {
+  collection,
+  query,
+  orderBy,
+  getDocs
+} from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
+
+import {
+  firebaseAvailable,
+  firestore
+} from './firebase-loader.js';
+
+console.log('✅ gallery-firebase.js cargado');
+alert('gallery-firebase.js cargado correctamente');
