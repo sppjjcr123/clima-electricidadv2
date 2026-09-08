78913b6c9cf21ad810566eb7765f91dbf7b1ae7e
sppjjcr123 <sppjjcr@gmail.com>
2026-09-07 20:06:58 -0400

    Agregar configuración Firebase para producción
    - Publicada configuración web requerida por GitHub Pages
    - Corregido error 404 de firebase-config.js
    - Habilitada carga dinámica de la galería en producción

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCZASHvWewYYEwhnXCSXd-5PRjk-gsg3M",
+  authDomain: "climaelectricidadgaleria.firebaseapp.com",
+  projectId: "climaelectricidadgaleria",
+  storageBucket: "climaelectricidadgaleria.firebasestorage.app",
+  messagingSenderId: "371914128978",
+  appId: "1:371914128978:web:e82454e1757a2e291e698b"
+};
+
+export default firebaseConfig;
